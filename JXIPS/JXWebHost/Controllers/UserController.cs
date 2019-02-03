d5bf7e32e5c5d0a9a8e7ecbecd4c4af19564571f
lixiong24 <442106890@qq.com>
2019-02-03 19:20:29 +0800

    完善会员模块相关内容

--- JXIPS/JXWebHost/Controllers/UserController.cs
+++ JXIPS/JXWebHost/Controllers/UserController.cs
@@ -11,8 +11,6 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace JXWebHost.Controllers
 {
 	[Authorize]
--- JXIPS/JXWebHost/Models/UserViewModels/LoginViewModel.cs
+++ JXIPS/JXWebHost/Models/UserViewModels/LoginViewModel.cs
@@ -8,13 +8,16 @@ namespace JXWebHost.Models.UserViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name ="用户名")]
 		[Required(ErrorMessage = "会员名不能为空")]
 		public string UserName { get; set; }
 
+        [Display(Name ="密码")]
 		[Required(ErrorMessage = "密码不能为空")]
 		[DataType(DataType.Password)]
 		public string UserPassword { get; set; }
 
-		public string ValidateCode { get; set; }
+        [Display(Name = "图形验证码")]
+        public string ValidateCode { get; set; }
 	}
 }
